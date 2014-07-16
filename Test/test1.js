344870f07d333b2089587ecd7aa5a876e5f6abfe
khushboo <khushboo@improwised.com>
2014-07-16 04:03:56 -0700

    movies modules and tests

---
+++ Test/test1.js
@@ -0,0 +1,72 @@
+var fs = require("fs");
+var path = "/Users/khushbu/mochaex/movies.dat";
+var assert = require('assert');
+var db = require("/Users/khushbu/mochaex/parsers/config.js")
+var file = require('/Users/khushbu/mochaex/parsers/map.js')
+
+describe('parsing movie file', function () {
+    it('should parse the line', function () {
+        file.parse(path, function (err, line) {
+
+            //  console.log(line)
+            assert.deepEqual(['3::Grumpier Old Men (1995)::Comedy|Romance',
+                '4::Waiting to Exhale (1995)::Comedy|Drama|Romance'
+            ], line);
+        });
+    });
+});
+
+describe('creating database', function () {
+    it('should create database', function (done) {
+        file.save(path, function (err, value) {
+            if (err) {
+                return done(err)
+            }
+            db.movies.get(3, function (err, value) {
+                if (err)
+                    done(err)
+                assert.equal(value, 'Grumpier Old Men (1995)::Comedy|Romance');
+                done()
+            })
+        });
+
+    });
+});
+
+
+describe('mapping cat to movid', function () {
+    it('should gives movies acc. to cat', function (done) {
+        file.mapping(path, function (err, map) {
+            if (err)
+                return done(err)
+            result = {
+                Comedy: ['3', '4'],
+                Romance: ['3', '4'],
+                Drama: ['4']
+             };
+             //console.log(map.Comedy);
+            assert.deepEqual(result, map)
+            done()
+        })
+    });
+});
+
+
+
+// describe('mapping cat to movid', function () {
+//     it('should gives movies acc. to cat', function (done) {
+//         file.mapping(["3::Grumpier Old Men (1995)::Comedy|Romance",
+//             "4::Waiting to Exhale (1995)::Comedy|Drama|Romance"
+//         ], function (err, map) {
+//             if (err)
+//                 return done(err)
+//             result = {
+//                 Comedy: ['3', '4'],
+//                 Romance: ['3', '4'],
+//                 Drama: ['4']
+//             };console.log(map.Comedy);
+//             assert.deepEqual(result, map)
+//             done()
+//         })
+//     });
+// });
