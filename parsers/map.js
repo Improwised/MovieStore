95581c625b6674cc16196e11f6d84ff2b1c4a753
khushboo <khushboo@improwised.com>
2014-07-16 07:13:12 -0700

    async

--- parsers/map.js
+++ parsers/map.js
@@ -1,5 +1,6 @@
 var db = require("../parsers/config.js")
 var fs = require("fs");
+var async = require('async')
 
 exports.parse = function (path, callback) {
     fs.readFile(path, function (error, data) {
@@ -24,52 +25,93 @@ exports.save = function (path, cb) {
         }
         var films = data.toString().split('\n')
         films.pop();
-        for (var j in films) {
-            var film = films[j].split("::");
+
+        async.each(films, function (line, callback) {
+            var film = line.split("::");
             var key = film[0];
             //console.log(key,films[j])
             var value = film[1] + "::" + film[2];
             db.movies.put(key, value);
-        }
-        cb();
-        // db.movies.put(key, value, cb);
-        // db.movies.get('4',function (err, value) {
-        //   console.log(4,value,"0000");
-        // })
-
-    });
-};
-
-exports.mapping = function (path, callback) {
-  var movidcatmap = {};
-  fs.readFile(path, function (error, data) {
-      if (error) {
-          return error
-      }
-      var films = data.toString().split('\n')
-      films.pop();
-
-      films.forEach(function (line) {
-        var splits = line.split("::")
-        var movid = splits[0];
-        var cat = splits[2]
-        var movicat = cat.split("|")
-            //  console.log(movicat[0]);
-        for (var i = 0; i < movicat.length; i++) {
-            if (movidcatmap[movicat[i]]) {
-
-                movidcatmap[movicat[i]].push(movid);
-                //console.log(movicat[i]+movidcatmap[movicat[i]])
-                db.genres.put(movicat[i],movidcatmap[movicat[i]])
-            } else {
-
-                movidcatmap[movicat[i]] = [movid];
-              //console.log(movicat[i]+movidcatmap[movicat[i]])
-                db.genres.put(movicat[i],movidcatmap[movicat[i]])
+            //console.log(key, value)
+            callback();
+        }, function (err) {
+            if (err) {
+                throw err;
             }
-
-        }
-    })
-callback(null, movidcatmap);
-  })
+        });
+    });
+    cb();
 }
+
+exports.mapping = function (path, cb) {
+        var movidcatmap = {};
+        fs.readFile(path, function (error, data) {
+          if (error) {
+              return error
+          }
+          var films = data.toString().split('\n')
+          films.pop();
+
+          async.each(films, function (line, callback) {
+            var splits = line.split("::")
+            var movid = splits[0];
+            var cat = splits[2]
+            var movicat = cat.split("|")
+                //  console.log(movicat[0]);
+            for (var i = 0; i < movicat.length; i++) {
+                if (movidcatmap[movicat[i]]) {
+
+                    movidcatmap[movicat[i]].push(movid);
+                    //console.log(movicat[i]+movidcatmap[movicat[i]])
+                    db.genres.put(movicat[i], movidcatmap[movicat[i]])
+                } else {
+
+                    movidcatmap[movicat[i]] = [movid];
+                    //console.log(movicat[i]+movidcatmap[movicat[i]])
+                    db.genres.put(movicat[i], movidcatmap[movicat[i]])
+                }
+
+            }},
+            function (err) {
+                if (err) {
+                    throw err;
+                }
+            }); cb(null, movidcatmap);
+          });
+  }
+            /*
+
+            exports.mapping = function (path, callback) {
+                var movidcatmap = {};
+                fs.readFile(path, function (error, data) {
+                    if (error) {
+                        return error
+                    }
+                    var films = data.toString().split('\n')
+                    films.pop();
+
+                    films.forEach(function (line) {
+                        var splits = line.split("::")
+                        var movid = splits[0];
+                        var cat = splits[2]
+                        var movicat = cat.split("|")
+                            //  console.log(movicat[0]);
+                        for (var i = 0; i < movicat.length; i++) {
+                            if (movidcatmap[movicat[i]]) {
+
+                                movidcatmap[movicat[i]].push(movid);
+                                //console.log(movicat[i]+movidcatmap[movicat[i]])
+                                db.genres.put(movicat[i], movidcatmap[movicat[i]])
+                            } else {
+
+                                movidcatmap[movicat[i]] = [movid];
+                                //console.log(movicat[i]+movidcatmap[movicat[i]])
+                                db.genres.put(movicat[i], movidcatmap[movicat[i]])
+                            }
+
+                        }
+                    })
+                    callback(null, movidcatmap);
+                })
+            }
+            */
